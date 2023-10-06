900713e9b93f74120738644bf5ab69a4308c0038
Rainer Flores <rainer.flores64@gmail.com>
2023-10-06 12:56:26 -0400

    added types for state app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,74 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useReducer } from 'react'
+import { Action, type State} from './types.d'
+
+const initialState: State = {
+  fromLanguage : '',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+function reducer(state: State, action: Action){
+  const {type} = action
+  if(type === 'INTERCHANGE_LANGUAGES'){
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }  
+
+  if(type === 'SET_FROM_LANGUAGE'){
+      return {
+        ...state,
+        fromLanguage: action.payload
+      }
+  }
+
+  if(type === 'SET_TO_LANGUAGE'){
+      return {
+        ...state,
+        toLanguage: action.payload
+      }
+  }
+
+  if(type === 'SET_FROM_TEXT'){
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if(type === 'SET_RESULT'){
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    }
+  }
+  return state
+}
 
 function App() {
+  const [{
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading},
+     dispatch] = useReducer(reducer,initialState)
+
+  console.log(fromLanguage)   
   return (
     <>
     <h1>Google Translate</h1>
+    <button onClick={()=> dispatch({type:'SET_FROM_LANGUAGE', payload: 'es'})}>change language</button>
+
     </>
   )
 }
