0a9288b259b8f00f4d6c6afc20a11f7f32b0e845
Rainer Flores <rainer.flores64@gmail.com>
2023-10-08 20:26:20 -0400

    added bootstrap layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { Container, Row, Col } from 'react-bootstrap'
 import { useStore } from './hooks/useStore'
+import { AUTO_LANGUAGE } from './constants'
 
 
 
 function App() {
-  const { setFromLanguage,fromLanguage }= useStore()
+  const { setFromLanguage,interchageLanguages,fromLanguage, toLanguage } = useStore()
+  console.log(fromLanguage)
+  console.log(toLanguage)
   return (
-    <>
-    <h1>Google Translate</h1>
-    <button onClick={()=> setFromLanguage('es')}>change language</button>
-      {fromLanguage}
-    </>
+    <Container fluid>
+      <h1>Google Translate</h1>
+      <Row>
+        <Col>
+           <h2>From</h2>
+          {fromLanguage}
+        </Col>
+        <Col>
+          <button
+            disabled={fromLanguage === AUTO_LANGUAGE} 
+            onClick={() => interchageLanguages()}
+          >
+            Interchange
+          </button>
+        </Col>
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+
+    </Container>
   )
 }
 
