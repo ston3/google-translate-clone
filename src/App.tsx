452cb6f28c66d33e348292dd21af38969ad52dcb
Rainer Flores <rainer.flores64@gmail.com>
2023-11-12 00:26:50 -0400

    added handled options

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import { useStore } from './hooks/useStore'
 import { AUTO_LANGUAGE } from './constants'
+import { ArrowIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
+import { SectionType } from './types.d'
 
 
 
 function App() {
-  const { setFromLanguage,interchageLanguages,fromLanguage, toLanguage } = useStore()
+  const { setFromLanguage, setToLanguage, interchageLanguages,fromLanguage, toLanguage } = useStore()
   console.log(fromLanguage)
   console.log(toLanguage)
+
   return (
     <Container fluid>
       <h1>Google Translate</h1>
       <Row>
         <Col>
-           <h2>From</h2>
+        <LanguageSelector
+          type = {SectionType.From}
+          value={fromLanguage}
+         onChange={setFromLanguage}
+         />
           {fromLanguage}
         </Col>
         <Col>
-          <button
+          <Button
+            variant='link'
             disabled={fromLanguage === AUTO_LANGUAGE} 
             onClick={() => interchageLanguages()}
           >
-            Interchange
-          </button>
+            < ArrowIcon/>
+  
+          </Button>
         </Col>
         <Col>
-          <h2>To</h2>
+          <LanguageSelector
+            type={SectionType.To}
+            value={toLanguage}
+            onChange={setToLanguage}
+          />
           {toLanguage}
         </Col>
       </Row>
--- src/types.d.ts
+++ src/types.d.ts
@@ -4,8 +4,8 @@ export type Language = keyof typeof SUPPORTED_LANGUAGES
 export type AutoLanguage = typeof AUTO_LANGUAGE
 export type FromLanguage = Language | AutoLanguage
 export interface State {
-    fromLanguage : string;
-    toLanguage: string;
+    fromLanguage : FromLanguage;
+    toLanguage: Language;
     fromText: string;
     result: string;
     loading: boolean
@@ -13,7 +13,13 @@ export interface State {
 
 export type Action = 
     | {type: 'INTERCHANGE_LANGUAGES'}
-    | {type: 'SET_FROM_LANGUAGE', payload: string}
-    | {type: 'SET_TO_LANGUAGE', payload: string}
+    | {type: 'SET_FROM_LANGUAGE', payload: FromLanguage}
+    | {type: 'SET_TO_LANGUAGE', payload: Language}
     | {type: 'SET_FROM_TEXT', payload: string}
     | {type: 'SET_RESULT', payload: string}
+
+
+export enum SectionType {
+    From = 'from',
+    To = 'to'
+}
